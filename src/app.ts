4a90ef8be3f1fa47219eaba43867a3443c2cbaa7
Angelique B <angelique@angelbal.com>
2024-04-21 17:50:14 +0100

    Move user-specific endpoints to /restaurants

--- src/app.ts
+++ src/app.ts
@@ -36,11 +36,13 @@ class App {
     }
 
     routes() {
+        // User-specific routes
+        this.server.use("/restaurants", routers.user);
+
         // Restaurant routes
         this.server.use("/restaurants", routers.restaurant);
         this.server.use("/restaurants/:restaurantId/menus/", routers.menu);
         this.server.use("/restaurants/:restaurantId/menus/:menuId/items", routers.item);
-        this.server.use("/", routers.user);
 
         // Internal routes
         this.server.use("/internal", routers.internal);
